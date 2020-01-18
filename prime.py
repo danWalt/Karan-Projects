13bb2ead4d626407a2261982d4cfc325f1518217
Daniel Walters <daniel.walters2110@gmail.com>
2020-01-18 10:50:51 +0200

    prime factor

---
+++ prime.py
@@ -0,0 +1,13 @@
+print('Pick a number you want to see the prime factorization of:')
+n = int(input())
+listofprimes = []
+i = 2
+while i * i <= n:
+    if n % i:
+        i += 1
+    else:
+        listofprimes.append(int(i))
+        n //= i
+if n > 1:
+    listofprimes.append(int(n))
+print(listofprimes)
